938748817dbe82f0f95940fcdc24fd22242e88a4
Andsirpa <andsirpa94@gmail.com>
2024-01-29 15:44:38 +0100

    add message and img

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Quack quack!',
+            myImage: 'https://ilmondo-rivista.it/wp-content/uploads/2023/08/shutterstock_1729553983-1000x500.jpg?crop=1'
         }
     }
 }).mount('#app')
